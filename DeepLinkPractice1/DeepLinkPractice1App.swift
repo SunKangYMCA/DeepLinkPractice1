be7f057660ef1836cdfcf8c1f0b2ab4d1c2e3e31
SunKang <coolshark248@gmail.com>
2024-06-27 15:46:10 -0600

    add basic

---
+++ DeepLinkPractice1/DeepLinkPractice1App.swift
@@ -0,0 +1,17 @@
+//
+//  DeepLinkPractice1App.swift
+//  DeepLinkPractice1
+//
+//  Created by 강성찬 on 2024-06-27.
+//
+
+import SwiftUI
+
+@main
+struct DeepLinkPractice1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
