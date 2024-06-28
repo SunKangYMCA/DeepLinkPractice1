033ba68d779e20761cc1d8a78d5c91b57f9f2fb7
SunKang <coolshark248@gmail.com>
2024-06-27 19:43:12 -0600

    add DeepLinkManager

---
+++ DeepLinkPractice1/Model/DataSource.swift
@@ -0,0 +1,8 @@
+//
+//  DataSource.swift
+//  DeepLinkPractice1
+//
+//  Created by 강성찬 on 2024-06-27.
+//
+
+import Foundation
---
+++ DeepLinkPractice1/Model/Enum/ParisCafeTab.swift
@@ -0,0 +1,8 @@
+//
+//  ParisCafeTab.swift
+//  DeepLinkPractice1
+//
+//  Created by 강성찬 on 2024-06-27.
+//
+
+import Foundation
